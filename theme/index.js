f4b828b3b9260d8fc431ce403f906f13ea77fa28
Antonin Nhek <a.nhek@icloud.com>
2021-08-30 17:30:09 +0200

    fixed: mistake on importing the component theme for chakra ui

--- theme/index.js
+++ theme/index.js
@@ -3,10 +3,10 @@ import { extendTheme } from '@chakra-ui/react';
 
 import breakpoints from './breakpoints';
 import colors from './colors';
-import button from './components/button';
-import heading from './components/heading';
-import link from './components/link';
-import text from './components/text';
+import Button from './components/button';
+import Heading from './components/heading';
+import Link from './components/link';
+import Text from './components/text';
 import fonts from './fonts';
 import styles from './styles';
 import textStyles from './textStyles';
@@ -18,10 +18,10 @@ const overrides = {
   fonts,
   textStyles,
   components: {
-    button,
-    heading,
-    link,
-    text,
+    Button,
+    Heading,
+    Link,
+    Text,
   },
 };
 
