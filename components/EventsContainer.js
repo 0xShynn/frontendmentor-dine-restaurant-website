7a1a6d3881f6f7d9ac455aa576d6ffbecd4a4045
Antonin Nhek <a.nhek@icloud.com>
2021-08-31 14:47:40 +0200

    added: pattern line on tab image

--- components/EventsContainer.js
+++ components/EventsContainer.js
@@ -8,13 +8,17 @@ import {
   TabPanels,
   Tabs,
   Text,
+  useBreakpointValue,
 } from '@chakra-ui/react';
 import NextImage from 'next/image';
 import PropTypes from 'prop-types';
 
 import familyGatheringMobile from '../assets/homepage/family-gathering-mobile@2x.jpg';
+import familyGatheringTablet from '../assets/homepage/family-gathering-tablet@2x.jpg';
 import socialEventsMobile from '../assets/homepage/social-events-mobile@2x.jpg';
+import socialEventsTablet from '../assets/homepage/social-events-tablet@2x.jpg';
 import specialEventsMobile from '../assets/homepage/special-events-mobile@2x.jpg';
+import specialEventsTablet from '../assets/homepage/special-events-tablet@2x.jpg';
 
 import CustomLink from './CustomLink';
 
@@ -25,34 +29,49 @@ const EventsContainer = () => {
       content:
         'We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We’ll provide a memorable experience for all.',
       imageMobile: familyGatheringMobile,
+      imageTablet: familyGatheringTablet,
     },
     {
       label: 'Special Events',
       content:
         'Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you. We’ll be sure to mark your special date with an unforgettable meal.',
       imageMobile: specialEventsMobile,
+      imageTablet: specialEventsTablet,
     },
     {
       label: 'Social Events',
       content:
         'Are you looking to have a larger social event? No problem! We’re more than happy to cater for big parties. We’ll work with you to make your event a hit with everyone.',
       imageMobile: socialEventsMobile,
+      imageTablet: socialEventsTablet,
     },
   ];
 
   function DataTabs({ data }) {
+    const tabsOrientation = useBreakpointValue({
+      base: 'vertical',
+      md: 'horizontal',
+    });
+
     return (
-      <Tabs isFitted orientation="vertical" variant="unstyled" id="1" py="16">
+      <Tabs
+        isFitted
+        orientation={tabsOrientation}
+        variant="unstyled"
+        id="1"
+        py="16"
+      >
         <Flex direction="column" align="center">
           <Flex>
             <TabPanels>
               {data.map((tab, index) => (
                 <TabPanel key={index}>
+                  {/* image mobile version (base, sm) */}
                   <Box
                     w={{ base: 'full', sm: '327px' }}
-                    display="inline-flex"
                     h={{ base: 'full', sm: '400px' }}
                     shadow="2xl"
+                    display={{ base: 'inline-flex', md: 'none' }}
                   >
                     <NextImage
                       src={tab.imageMobile}
@@ -60,12 +79,39 @@ const EventsContainer = () => {
                       height="400px"
                     />
                   </Box>
+
+                  {/* image tablet version (md) */}
+                  <Box
+                    w="573px"
+                    h="360px"
+                    shadow="2xl"
+                    display={{ base: 'none', md: 'block' }}
+                    pos="relative"
+                  >
+                    <NextImage
+                      src={tab.imageTablet}
+                      width="573px"
+                      height="360px"
+                    />
+                    <Box
+                      pos="absolute"
+                      top="-38px"
+                      left="-60px"
+                      w="160px"
+                      h="76px"
+                    >
+                      <NextImage
+                        src="/images/pattern-lines.svg"
+                        layout="fill"
+                      />
+                    </Box>
+                  </Box>
                 </TabPanel>
               ))}
             </TabPanels>
           </Flex>
 
-          <TabList pt="4" pb="2">
+          <TabList pt="4" pb="2" w={{ md: '90vw', lg: '70vw' }}>
             {data.map((tab, index) => (
               <Tab
                 key={index}
