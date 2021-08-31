1c101e6f7f2a6e4d3f8995629615946031f123bd
Antonin Nhek <a.nhek@icloud.com>
2021-08-31 09:32:38 +0200

    updated: footer (md/lg)

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import DineLogo from '../assets/brand/DineLogo';
 const Footer = () => {
   const TextBlock = ({ children }) => (
     <Box
-      textAlign="center"
+      textAlign={{ base: 'center', md: 'left' }}
       textTransform="uppercase"
       mb="6"
       fontSize="14px"
@@ -16,25 +16,34 @@ const Footer = () => {
   );
   return (
     <Box role="contentinfo" bg="primary.codgray">
-      <Flex color="white" align="center" direction="column" px="6" py="16">
-        <Box w="103px" h="40px" mb="10">
+      <Flex
+        color="white"
+        direction={{ base: 'column', md: 'row' }}
+        align={{ base: 'center', md: 'flex-start' }}
+        justify={{ md: 'center' }}
+        px={{ base: 6, md: 16 }}
+        py="16"
+      >
+        <Box w="103px" h="40px" mb="10" mr={{ md: 32 }}>
           <DineLogo />
         </Box>
 
-        <TextBlock>
-          Marthwaite, Sedbergh
-          <br />
-          Cumbria
-          <br />
-          +00 44 123 4567
-          <br />
-        </TextBlock>
+        <Box>
+          <TextBlock>
+            Marthwaite, Sedbergh
+            <br />
+            Cumbria
+            <br />
+            +00 44 123 4567
+            <br />
+          </TextBlock>
 
-        <TextBlock>
-          <Text>OPEN TIMES </Text>
-          <Text>MON - FRI: 09:00 AM - 10:00 PM</Text>
-          <Text>SAT - SUN: 09:00 AM - 11:30PM</Text>
-        </TextBlock>
+          <TextBlock>
+            <Text>OPEN TIMES </Text>
+            <Text>MON - FRI: 09:00 AM - 10:00 PM</Text>
+            <Text>SAT - SUN: 09:00 AM - 11:30PM</Text>
+          </TextBlock>
+        </Box>
       </Flex>
     </Box>
   );
